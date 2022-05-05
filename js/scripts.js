b2da641e190b4f51955b6964e3285094675de600
DmitriCzinovskij <92273005+DmitriCzinovskij@users.noreply.github.com>
2022-05-05 12:01:31 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+"use strict"
+
+
+for (let step = 1; step < 5; step++) {
+ console.log(step);
+}
+let teach = 'freelance'
+console.log(teach);
+let username = 'Вася'
+let user
+user = username
+console.log(user);
+alert('ЛЯ, ДЭБИЛЯРА!')
+console.log(alert);
